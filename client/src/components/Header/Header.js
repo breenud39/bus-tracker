029fef5527656810c333fafd2caf602d16cb8fc9
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-03-23 23:07:02 -0400

    added navigation routes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,22 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Nav } from "../Nav/Nav";
-import {Hero} from "../Hero/Hero";
-
-
+import { Hero } from "../Hero/Hero";
+import { Routes, Router, Link, Outlet } from "react-router";
 
 const Header = () => {
+  const { pages = [Home, About], setCurrentPage, currentPage } = props;
+
   return (
-    <header className="">
-      <div className="">
+    <div>
+      <header>
         <Link className="text-light" to="/">
           <h1 className="m-0">Bus Tracker</h1>
         </Link>
-        
-
-        <p className="m-0"></p>
-      </div>
-    </header>
+        <nav>
+          <Routes path="/">
+            <Route index element={<Home />}></Route>
+            <Route path="trip-planner" element={<TripPlanner />}></Route>
+            <Route
+              path="routes-and-schedules"
+              element={<RoutesAndSchedules />}
+            ></Route>
+            <Route path="fares-and-passes" element={<FaresAndPasses />}></Route>
+            <Route
+              path="service-advisories"
+              element={<serviceAdvisories />}
+            ></Route>
+            <Route path="contact" element={<Contact />}></Route>
+            <Route path="Login" element={<Login />}></Route>
+          </Routes>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/trip-planner">Trip Planner</Link>
+            </li>
+            <li>
+              <Link to="/routes-and-schedules">Routes And Schedules</Link>
+            </li>
+            <li>
+              <Link to="/fares-and-passes">Fares And Passes</Link>
+            </li>
+            <li>
+              <Link to="/service-advisories">Service And Advisories</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet></Outlet>
+    </div>
   );
 };
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {Hero} from "Hero";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Hero></Hero>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Home;
