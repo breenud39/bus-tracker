98e8e54efd0de0c19c81166bf049a3d6ac4cc02e
Vaggelis <evangelodamianos@gmail.com>
2022-03-23 20:56:34 -0400

    run apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 // 1. Import the extendTheme function
-import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 // import { Box, Flex, Stack, Grid, Wrap, AspectRatio } from "@chakra-ui/layout" --downloaded
 
 // 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-const theme = extendTheme({ colors })
 
 // 3. Pass the `theme` prop to the `ChakraProvider`
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   )
