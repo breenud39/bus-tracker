64bc708f6266e3e70ae1154e0f589f3000386f7a
Manning <farjad_uk@hotmail.com>
2022-03-29 16:41:35 -0400

    app.js import

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Register from './pages/Home'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
                     <Route path = '/' element = { <Dashboard />} />
                     <Route path = '/login' element = { <Login />} />
                     <Route path = '/register' element = { <Register />} />
+                    <Route path = '/home' element = { <Home />} />
                   </Routes>
             </div>
       </Router>
