726539b53236b549ac4ee6df7cfe8e78768c8855
Mekonnen Mohammed <90818220+Mekonnenke@users.noreply.github.com>
2022-03-28 20:34:08 -0400

    redux added

---
+++ client/src/components/BuyTicketItem.jsx
@@ -0,0 +1,18 @@
+import { useDispatch } from 'react-redux'
+import { deleteBuyTicket } from '../features/buyTickets/buyTicketSlice'
+
+function BuyTicketItem({ buyTicket }) {
+  const dispatch = useDispatch()
+
+  return (
+    <div className='buyTicket'>
+      <div>{new Date(buyTicket.createdAt).toLocaleString('en-US')}</div>
+      <h2>{buyTicket.text}</h2>
+      <button onClick={() => dispatch(deleteBuyTicket(buyTicket._id))} className='close'>
+        X
+      </button>
+    </div>
+  )
+}
+
+export default BuyTicketItem
