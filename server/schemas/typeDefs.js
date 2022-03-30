d892ffa9dbda710aac7d6a4a618dc8653795e6b8
Vaggelis <evangelodamianos@gmail.com>
2022-03-30 17:10:04 -0400

    added typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,95 +1,22 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const { gql } = require('apollo-server-express');
-=======
 // importing gql from the Apollo
 const { gql } = require("apollo-server-express");
->>>>>>> 61154fa (add authn and buy ticket)
-=======
-// importing gql from the Apollo
-const { gql } = require("apollo-server-express");
->>>>>>> 61154fa (add authn and buy ticket)
 
+// The GraphQL schema
 const typeDefs = gql`
-<<<<<<< HEAD
-<<<<<<< HEAD
-  type Category {
-    _id: ID
-    name: String
-  }
-
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    category: Category
-  }
-
-  type Order {
-    _id: ID
-    purchaseDate: String
-    products: [Product]
-  }
-
-  type User {
-    _id: ID
-    firstName: String
-    lastName: String
-    email: String
-    orders: [Order]
-  }
-
-  type Checkout {
-    session: ID
-  }
-
-  type Auth {
-    token: ID
-    user: User
-  }
-
-  type Query {
-    categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
-    user: User
-    order(_id: ID!): Order
-    checkout(products: [ID]!): Checkout
-  }
-
-  type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    addOrder(products: [ID]!): Order
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
-    updateProduct(_id: ID!, quantity: Int!): Product
-    login(email: String!, password: String!): Auth
-  }
-`;
-
-module.exports = typeDefs;
-=======
-=======
->>>>>>> 61154fa (add authn and buy ticket)
-type busRoot {
+type BusRoot {
   _id: ID
   name: String
 }
-type buyTicket {
+type BuyTicket {
   _id: ID
   name: String
-  description: String
-  image: String
-  quantity: Int
   price: Float
   busRoot: BusRoot
 }
 type Order {
   _id: ID
   purchaseDate: String
-  busTicket: [BusTicket]
+  buyTickets: [BuyTicket]
 }
 type User {
   _id: ID
@@ -103,26 +30,23 @@ type Auth {
 }
 type Query {
   busRoots: [BusRoot]
-  buyTickets(busRoot: ID, name: Int): [BusTicket]
-  busTicket(_id: ID!): PbusTicket
+  buyTickets(busRoot: ID, root: Int): [BuyTicket]
+  buyTicket(_id: ID!): BuyTicket
   user: User
   order(_id: ID!): Order
-  checkout(busTicketss: [ID]!): Checkout
+  checkout(buyTicket: [ID]!): Checkout
 }
 type Mutation {
   addUser(name: String!, email: String!, password: String!): Auth
   addOrder(buyTickets: [ID]!): Order
   updateUser(name: String, email: String, password: String): User
-  updatebuyTicket(_id: ID!, quantity: Int!): BuyTicket
+  updateBuyTicket(_id: ID!, quantity: Int!): BuyTicket
   login(email: String!, password: String!): Auth
 }
-
   
+type Checkout {
+    session: ID
+  }
 `;
 // export the typeDefs
-<<<<<<< HEAD
 module.exports = typeDefs;
->>>>>>> 61154fa (add authn and buy ticket)
-=======
-module.exports = typeDefs;
->>>>>>> 61154fa (add authn and buy ticket)
