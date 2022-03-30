970f52617b98c2d5c7e9942ff9ff4ee4c3df4f47
Manning <farjad_uk@hotmail.com>
2022-03-29 20:19:07 -0400

    tripPlanner

---
+++ client/src/components/TripPlanner.jsx
@@ -0,0 +1,12 @@
+function TripPlanner() {
+    const [formData, setFormData] = useState({
+        busName: '',
+        timeOfDep: '',
+        city: '',        
+        })
+
+  const {busName, timeOfDep, city} =formData
+
+  }
+  
+  export default TripPlanner
