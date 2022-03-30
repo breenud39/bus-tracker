9ad9ff7c99183aee2da5fced872b868282ad0540
Vaggelis <evangelodamianos@gmail.com>
2022-03-30 17:32:51 -0400

    added seeds

--- server/routes/api/busRoot.js
+++ server/routes/api/busRoot.js
@@ -5,10 +5,10 @@ const router = express.Router()
 
 router.get('/', (req, res) => { 
      resstatus(200).json({message: 'Show all roots'})
-})
+});
 
 router.getById('/:id', (req, res) => { 
     resstatus(200).json({message: 'Show only one root '})
-})
+});
 
 module.export = router
