fedad9bf91587019788b85d1b489dba347ae2aeb
Vaggelis <evangelodamianos@gmail.com>
2022-03-30 23:26:45 -0400

    branches merged

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bus-tracker', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bus-tracker-application', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
