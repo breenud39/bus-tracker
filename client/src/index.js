4569ef0932ace8047df6ed26bad6855937c2cfae
Vaggelis <evangelodamianos@gmail.com>
2022-03-31 16:53:10 -0400

    app/index.js fix

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -18,7 +19,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
