0346be69a6ce55b015de368102cde4ec717ef3fd
Vaggelis <evangelodamianos@gmail.com>
2022-04-01 14:57:23 -0400

    connection fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/bus-tracker',
-  {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-  }
-);
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
